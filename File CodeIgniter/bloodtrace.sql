a44cb61bf654b2fc5e1517b23f02ae473fc5707d
ikoarya <anggeriko.aryasena@gmail.com>
2015-09-07 16:03:13 +0700

    07/09/2015 16:02 DONE Penambahan SQL

--- File CodeIgniter/bloodtrace.sql
+++ File CodeIgniter/bloodtrace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 03, 2015 at 07:37 AM
+-- Generation Time: Sep 06, 2015 at 06:32 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -28,25 +28,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `aftap` (
   `id_aftap` int(11) NOT NULL AUTO_INCREMENT,
-  `no_aftap` varchar(30) DEFAULT NULL,
-  `nomor_donor` varchar(30) DEFAULT NULL,
-  `id_kantung` int(11) DEFAULT NULL,
-  `jenis_kantung` varchar(30) DEFAULT NULL,
-  `asal_darah` varchar(50) DEFAULT NULL,
+  `no_aftap` varchar(30) NOT NULL,
+  `nomor_donor` varchar(30) NOT NULL,
+  `id_kantung` int(11) NOT NULL,
+  `jenis_kantung` varchar(30) NOT NULL,
+  `asal_darah` varchar(50) NOT NULL,
   `tanggal_aftap` date DEFAULT NULL,
   `waktu_aftap` time DEFAULT NULL,
   `petugas_aftap` varchar(50) DEFAULT NULL,
   `keterangan` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id_aftap`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `aftap`
 --
 
 INSERT INTO `aftap` (`id_aftap`, `no_aftap`, `nomor_donor`, `id_kantung`, `jenis_kantung`, `asal_darah`, `tanggal_aftap`, `waktu_aftap`, `petugas_aftap`, `keterangan`) VALUES
-(1, NULL, NULL, 1302818, NULL, 'Dalam Gedung', NULL, '09:00:00', 'Arifah', NULL),
-(6, '12920150002', '101201500001', 14045, 'Single', 'UTD', '2015-09-02', '12:34:00', 'Afif', 'sukses');
+(1, '10509150001', '103151', 14022, 'Single', 'UTD', '2015-09-05', '23:19:00', 'Kiki', 'Sukses');
 
 -- --------------------------------------------------------
 
@@ -61,15 +60,29 @@ CREATE TABLE IF NOT EXISTS `detail_faktur` (
   `id_donor` varchar(30) DEFAULT NULL,
   `jenis` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id_detailfaktur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `detail_faktur`
 --
 
 INSERT INTO `detail_faktur` (`id_detailfaktur`, `nomor_faktur`, `id_kantung`, `id_donor`, `jenis`) VALUES
-(1, '23920150002', '14045', '101201500001', 'Single'),
-(2, '23920150002', '14045', '101201500001', 'Single');
+(3, '25920150001', '14022', '103151', 'Single'),
+(4, '25920150001', '14045', '103152', 'Single');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_pengiriman`
+--
+
+CREATE TABLE IF NOT EXISTS `detail_pengiriman` (
+  `id_detailkirim` int(11) NOT NULL AUTO_INCREMENT,
+  `sscc` varchar(30) DEFAULT NULL,
+  `id_produksi` varchar(30) DEFAULT NULL,
+  `id_kantung` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_detailkirim`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -85,23 +98,7 @@ CREATE TABLE IF NOT EXISTS `detail_permintaan` (
   `jumlahreq` int(11) NOT NULL,
   PRIMARY KEY (`id_detailminta`),
   KEY `nomorform` (`noform`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
-
---
--- Dumping data for table `detail_permintaan`
---
-
-INSERT INTO `detail_permintaan` (`id_detailminta`, `noform`, `jenisreq`, `goldarreq`, `jumlahreq`) VALUES
-(4, '98765', 'Washed Red Cells', 'AB', 2),
-(5, '98765', 'Fresh Plasma', 'O', 4),
-(6, '98765', 'Whole Blood', 'AB', 2),
-(7, '12345', 'Whole Blood', 'A', 2),
-(8, '12345', 'Fresh Plasma', 'O', 6),
-(9, '12345', 'Trombocyte Coencentrate', 'B', 4),
-(10, '12345', 'Washed Red Cells', 'A', 2),
-(11, '103785729001', 'Washed Red Cells', 'AB', 3),
-(12, '103785729001', 'Plasma', 'A', 2),
-(13, '103785729001', 'Washed Red Cells', 'O', 6);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -135,10 +132,7 @@ CREATE TABLE IF NOT EXISTS `donor` (
 --
 
 INSERT INTO `donor` (`id_donor`, `nomor_donor`, `nama`, `jenis_kelamin`, `agama`, `tanggal_lahir`, `tempat_lahir`, `pekerjaan`, `alamat`, `kodepos`, `wilayah`, `golongan_darah`, `rhesus`, `noid`, `jenid`, `jml_donor`, `terakhir_donor`) VALUES
-(1, '101201500001', 'Budi Doremi', 'Laki-laki', 'Islam', '2015-05-18', 'Surabaya', 'Pegawai', 'Keputih', '60111', 'Surabaya', 'A', 'Negatif', '123445678', 'KTP', 4, '2015-09-01'),
-(2, '101201500002', 'Riko', 'Laki-laki', 'Islam', '2015-08-05', 'Bantul', 'Pelajar', 'Keputih', '60111', 'Surabaya', 'A', NULL, '12345678', 'SIM', 2, '2015-03-16'),
-(3, '101201500003', 'Jono', 'Laki-laki', 'Islam', '2015-08-12', 'Jogja', 'Pelajar', 'Surabaya', '60111', 'Surabaya', 'B', NULL, '67891234', 'SIM', 5, '2015-03-10'),
-(4, '101201500004', 'Anggeriko', 'Laki-laki', 'Islam', '2015-08-06', 'Bantul', 'Pelajar', 'Keputih', '60111', 'Surabaya', 'A', NULL, '89883746', 'SIM', 2, '2015-06-01');
+(1, '103151', 'Anggeriko Aryasena', 'Laki-laki', 'Islam', '1994-08-25', 'Bantul', 'Pelajar', 'Jalan Kejawan Gebang', '60111', 'Keputih Surabaya', 'A', 'Positif', '3326102508940001', 'KTP', 1, '2015-09-05');
 
 -- --------------------------------------------------------
 
@@ -151,22 +145,24 @@ CREATE TABLE IF NOT EXISTS `faktur` (
   `nomor_faktur` varchar(30) DEFAULT NULL,
   `tgl_kirim` date DEFAULT NULL,
   `waktu_kirim` time DEFAULT NULL,
+  `tgl_terima` date DEFAULT NULL,
+  `waktu_terima` time DEFAULT NULL,
   `asal_darah` varchar(30) DEFAULT NULL,
   `petugas_kirim` varchar(50) DEFAULT NULL,
+  `petugas_terima` varchar(30) DEFAULT NULL,
   `waktu_prod` time DEFAULT NULL,
   `petugasprod` varchar(50) DEFAULT NULL,
   `waktu_imltd` time DEFAULT NULL,
   `petugas_imltd` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_faktur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `faktur`
 --
 
-INSERT INTO `faktur` (`id_faktur`, `nomor_faktur`, `tgl_kirim`, `waktu_kirim`, `asal_darah`, `petugas_kirim`, `waktu_prod`, `petugasprod`, `waktu_imltd`, `petugas_imltd`) VALUES
-(1, NULL, NULL, '11:56:00', NULL, 'Indah', '12:00:00', 'Kholil', '12:00:00', 'Mei P'),
-(2, '23920150002', '2015-09-03', '10:29:00', 'UTD Sidoarjo', 'Afif', NULL, NULL, NULL, NULL);
+INSERT INTO `faktur` (`id_faktur`, `nomor_faktur`, `tgl_kirim`, `waktu_kirim`, `tgl_terima`, `waktu_terima`, `asal_darah`, `petugas_kirim`, `petugas_terima`, `waktu_prod`, `petugasprod`, `waktu_imltd`, `petugas_imltd`) VALUES
+(1, '25920150001', '2015-09-05', '23:43:00', '2015-09-06', '06:04:00', 'UTD Sidoarjo', 'Fikri', 'Fikri', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -177,7 +173,7 @@ INSERT INTO `faktur` (`id_faktur`, `nomor_faktur`, `tgl_kirim`, `waktu_kirim`, `
 CREATE TABLE IF NOT EXISTS `kantung` (
   `id_kantung` varchar(30) NOT NULL,
   `nomor_donor` varchar(30) NOT NULL,
-  `id_faktur` varchar(30) NOT NULL,
+  `id_faktur` varchar(30) DEFAULT NULL,
   `jenis` varchar(50) NOT NULL,
   `petugas_imltd` varchar(50) NOT NULL,
   `tanggal_aftap` date DEFAULT NULL,
@@ -196,8 +192,7 @@ CREATE TABLE IF NOT EXISTS `kantung` (
 --
 
 INSERT INTO `kantung` (`id_kantung`, `nomor_donor`, `id_faktur`, `jenis`, `petugas_imltd`, `tanggal_aftap`, `waktu_aftap`, `tanggal_periksa`, `waktu_periksa`, `hiv`, `sifilis`, `hepatitis`, `hcv`) VALUES
-('1', '1', '1', 'Plasma', 'Arifah', NULL, NULL, NULL, '08:00:00', '0', '0', '0', '0'),
-('14045', '101201500001', '', 'Single', 'Riko', '2015-09-02', '12:34:00', '2015-09-03', '12:29:00', 'Negatif', 'Negatif', 'Negatif', 'Negatif');
+('14022', '103151', '25920150001', 'Single', 'Fikri', '2015-09-05', '23:19:00', '2015-09-06', '00:04:00', 'Negatif', 'Negatif', 'Negatif', 'Negatif');
 
 -- --------------------------------------------------------
 
@@ -240,15 +235,14 @@ CREATE TABLE IF NOT EXISTS `pendaftaran` (
   `petugas` varchar(50) DEFAULT NULL,
   `hasil` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_daftar`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `pendaftaran`
 --
 
 INSERT INTO `pendaftaran` (`id_daftar`, `no_pendaftaran`, `nomor_donor`, `tanggaldaftar`, `waktudaftar`, `rhesus`, `hb`, `bb`, `tensi`, `volume`, `petugas`, `hasil`) VALUES
-(1, '1011920150001', '101201500001', '2015-09-01', '08:49:00', 'Positif', 13, 68, '120/80', 500, 'Dudin', 'Lolos'),
-(2, '1011920150002', '101201500001', '2015-09-01', '12:00:00', 'Positif', 13, 57, '120/80', 500, 'Fifi', 'Lolos');
+(1, '1030509151', '103151', '2015-09-05', '22:43:00', 'Positif', 13, 78, '120/85', 500, 'Fahri', 'Lolos');
 
 -- --------------------------------------------------------
 
@@ -292,14 +286,53 @@ CREATE TABLE IF NOT EXISTS `permintaan` (
   PRIMARY KEY (`noform`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `permintaan`
+-- Table structure for table `persediaan_darah`
 --
 
-INSERT INTO `permintaan` (`noform`, `tglpermintaan`, `koders`, `tujuan`, `petugas`) VALUES
-('103785729001', '2015-08-29', '103', 'PMI Jawa Timur', '0'),
-('12345', '2015-08-29', '103', 'PMI', '0'),
-('98765', '2015-08-28', '1234', 'rw', '0');
+CREATE TABLE IF NOT EXISTS `persediaan_darah` (
+  `id_persediaan` int(11) NOT NULL AUTO_INCREMENT,
+  `jenis` varchar(50) DEFAULT NULL,
+  `golongan_darah` varchar(30) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_persediaan`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
+
+--
+-- Dumping data for table `persediaan_darah`
+--
+
+INSERT INTO `persediaan_darah` (`id_persediaan`, `jenis`, `golongan_darah`, `jumlah`) VALUES
+(1, 'Whole Blood', 'A', 0),
+(2, 'Whole Blood', 'AB', 0),
+(3, 'Whole Blood', 'B', 0),
+(4, 'Whole Blood', 'O', 0),
+(5, 'Packed Red Cells', 'A', 0),
+(6, 'Packed Red Cells', 'AB', 0),
+(7, 'Packed Red Cells', 'B', 0),
+(8, 'Packed Red Cells', 'O', 0),
+(9, 'Plasma', 'A', 1),
+(10, 'Plasma', 'AB', 0),
+(11, 'Plasma', 'B', 0),
+(12, 'Plasma', 'O', 0),
+(13, 'Fresh Plasma', 'A', 0),
+(14, 'Fresh Plasma', 'AB', 0),
+(15, 'Fresh Plasma', 'B', 0),
+(16, 'Fresh Plasma', 'O', 0),
+(17, 'Trombocyte Concentrate', 'A', 0),
+(18, 'Trombocyte Concentrate', 'AB', 0),
+(19, 'Trombocyte Concentrate', 'B', 0),
+(20, 'Trombocyte Concentrate', 'O', 0),
+(21, 'Washed Red Cell', 'A', 0),
+(22, 'Washed Red Cell', 'AB', 0),
+(23, 'Washed Red Cell', 'B', 0),
+(24, 'Washed Red Cell', 'O', 0),
+(25, 'Anti Haemophlic Factor Concentrate', 'A', 0),
+(26, 'Anti Haemophlic Factor Concentrate', 'AB', 0),
+(27, 'Anti Haemophlic Factor Concentrate', 'B', 0),
+(28, 'Anti Haemophlic Factor Concentrate', 'O', 0);
 
 -- --------------------------------------------------------
 
@@ -309,6 +342,7 @@ INSERT INTO `permintaan` (`noform`, `tglpermintaan`, `koders`, `tujuan`, `petuga
 
 CREATE TABLE IF NOT EXISTS `produksi` (
   `id_produksi` int(11) NOT NULL AUTO_INCREMENT,
+  `barcode_produksi` varchar(30) DEFAULT NULL,
   `id_kantung` int(11) NOT NULL,
   `sscc` varchar(50) NOT NULL,
   `jenis_komponen` varchar(50) NOT NULL,
@@ -316,16 +350,14 @@ CREATE TABLE IF NOT EXISTS `produksi` (
   `waktu_kadaluarsa` date NOT NULL,
   `petugas_produksi` varchar(50) NOT NULL,
   PRIMARY KEY (`id_produksi`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `produksi`
 --
 
-INSERT INTO `produksi` (`id_produksi`, `id_kantung`, `sscc`, `jenis_komponen`, `waktu_produksi`, `waktu_kadaluarsa`, `petugas_produksi`) VALUES
-(1, 1, '12345', 'Whole Blood', '2015-08-12', '2015-08-28', 'Arifah'),
-(2, 2, '9876', 'Plasma', '2015-08-29', '2015-09-30', 'Riko'),
-(3, 3, '9876', 'Whole Blood', '2015-08-29', '2015-08-25', 'Riki');
+INSERT INTO `produksi` (`id_produksi`, `barcode_produksi`, `id_kantung`, `sscc`, `jenis_komponen`, `waktu_produksi`, `waktu_kadaluarsa`, `petugas_produksi`) VALUES
+(6, '10314022003', 14022, '', 'Plasma', '2015-09-06', '2015-09-27', 'Fifi');
 
 -- --------------------------------------------------------
 
@@ -334,6 +366,7 @@ INSERT INTO `produksi` (`id_produksi`, `id_kantung`, `sscc`, `jenis_komponen`, `
 --
 
 CREATE TABLE IF NOT EXISTS `shipping` (
+  `id_pengiriman` int(11) NOT NULL AUTO_INCREMENT,
   `sscc` int(11) NOT NULL,
   `status` varchar(50) DEFAULT NULL,
   `waktu_kirimship` date DEFAULT NULL,
@@ -344,16 +377,17 @@ CREATE TABLE IF NOT EXISTS `shipping` (
   `suhu_terima` int(11) DEFAULT NULL,
   `kode_tujuan` int(11) DEFAULT NULL,
   `kode_supplier` int(11) DEFAULT NULL,
-  PRIMARY KEY (`sscc`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`sscc`),
+  UNIQUE KEY `id_pengiriman` (`id_pengiriman`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `shipping`
 --
 
-INSERT INTO `shipping` (`sscc`, `status`, `waktu_kirimship`, `waktu_terimaship`, `petugas_kirimship`, `petugas_terimaship`, `suhu_kirim`, `suhu_terima`, `kode_tujuan`, `kode_supplier`) VALUES
-(9876, 'Baik', '2015-08-29', NULL, 'Riki', NULL, 23, NULL, 104, NULL),
-(12345, 'Baik', '2015-08-22', '2015-08-24', 'Arina', 'Arifah', 20, 20, 111, 222);
+INSERT INTO `shipping` (`id_pengiriman`, `sscc`, `status`, `waktu_kirimship`, `waktu_terimaship`, `petugas_kirimship`, `petugas_terimaship`, `suhu_kirim`, `suhu_terima`, `kode_tujuan`, `kode_supplier`) VALUES
+(1, 9876, 'Baik', '2015-08-29', NULL, 'Riki', NULL, 23, NULL, 104, NULL),
+(2, 12345, 'Baik', '2015-08-22', '2015-08-24', 'Arina', 'Arifah', 20, 20, 111, 222);
 
 -- --------------------------------------------------------
 
